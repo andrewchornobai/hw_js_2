1806e09cf182249292ee9485bc11acd6e286a7e6
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 09:48:52 +0200

    added hw 2

---
+++ script.js
@@ -0,0 +1,120 @@
+// zavdannia 1
+// let correct = 0;
+// let mistake = 0; 
+// let b = prompt('Скільки буде 2+2?');
+// if(b==4){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let c = prompt('Скільки буде 2*12?');
+// if(c==24){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let d = prompt('Столиця україни?');
+// if(d == 'Київ'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let e = prompt('Столиця моди?');
+// if(e == 'Париж'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let f = prompt('Марка авто на три літери?(всі з великої літери)');
+// if(f == 'BMW'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let g = prompt('Україська марка авто на чотири літери?(всі з великої літери)');
+// if(g == 'КРАЗ'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let h = prompt('Найвідоміший фільм із Арнольдом Шварцнеггером?');
+// if(h == 'Термінатор'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let f = prompt('Найвищий водоспад світу?');
+// if(f == 'Анхель'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let k = prompt('Актор що грає Джеймс Бонда?(імя прізвище)');
+// if(k == 'Том Круз'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+// let s = prompt('Найвідоміший актор смішного німого кіно?');
+// if(s == 'Чарлі Чаплін'){
+//     correct++;
+// }else{
+//     mistake++;
+// }
+
+// console.log(correct);
+// console.log(mistake);
+
+// if(correct == 0){
+//     console.log("Немовля");
+// }else if(0 < correct >= 3){
+//     console.log("Почтаківець");
+// }else if(3 < correct >= 6){
+//     console.log("Любитель кросвордів");
+// }else if(6 < correct >= 9){
+//     console.log("Ботанік");
+// }else if(9 <correct >= 10){
+//     console.log("Король кросфордів");
+// }
+
+// zavdannia 2
+
+// let a = prompt('Введіть імя');
+// if(a == 'імя'){
+//     let b = prompt('Введіть пароль');
+// }else if(a == null){
+//     console.log('Вхід скасований');
+// }else{
+//     console.log("Я вас не знаю");
+// }
+
+// zavdannia 3
+
+// let a = +prompt('Введіть число від 1-12');
+// if(a==1){
+//     alert('Зима');
+// }else if(a==2){
+//     alert('Зима');
+// }else if(a==3){
+//     alert('Весна');
+// }else if(a==4){
+//     alert('Весна');
+// }else if(a==5){
+//     alert('Весна');
+// }else if(a==6){
+//     alert('Літо');
+// }else if(a==7){
+//     alert('Літо');
+// }else if(a==8){
+//     alert('Літо');
+// }else if(a==9){
+//     alert('Осінь');
+// }else if(a==10){
+//     alert('Осінь');
+// }else if(a==11){
+//     alert('Осінь');
+// }else if(a==12){
+//     alert('Зима');
+// }else{
+//     alert('Ви ввели не число або ж вийшли за межі діапазону');
+// }
